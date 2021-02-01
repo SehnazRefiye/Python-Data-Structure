bdcf2df5211059f11c97f6c8e72f2831d9c23997
Refiye Şehnaz Yıldırım <59395023+SehnazRefiye@users.noreply.github.com>
2021-02-02 00:41:49 +0300

    Add files via upload

---
+++ Recursive.py
@@ -0,0 +1,16 @@
+#factorial recursive
+def fact(num):
+    if num <= 1:
+        return 1
+    return num*fact(num-1)
+
+num = int(input("Please enter number: "))
+
+while True:
+    if num >= 0:
+        break
+    else:
+        num = int(input("Enter a big number greater than zero: "))
+result = fact(num)
+print(result)
+
